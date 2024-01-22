57c64947f39cba371b57c5316eea3f99ec0c0966
Abhi <60174597+abhiramv-sudo@users.noreply.github.com>
2024-01-22 16:12:33 +0530

    Update SyncTest.groovy
    changes

--- code/SyncTest.groovy
+++ code/SyncTest.groovy
@@ -4,7 +4,7 @@ procedure 'SyncTest', {
   timeLimit = '0'
 
   step 'TestSync', {
-    command = 'echo "Hello World 123455674444487878787"'
+    command = 'echo "Hello World 12345567444448787878799999"'
     procedureName = 'SyncTest'
     timeLimit = '0'
     timeLimitUnits = 'seconds'
