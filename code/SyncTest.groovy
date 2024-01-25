d75104fe465e2f8163ddc5dc6d3a8ed2f541702d
Abhi <60174597+abhiramv-sudo@users.noreply.github.com>
2024-01-25 15:28:54 +0530

    Update SyncTest.groovy

--- code/SyncTest.groovy
+++ code/SyncTest.groovy
@@ -4,7 +4,7 @@ procedure 'SyncTest', {
   timeLimit = '0'
 
   step 'TestSync', {
-    command = 'echo "Sync the webhook123"'
+    command = 'echo "Sync the webhook1234343434"'
     procedureName = 'SyncTest'
     timeLimit = '0'
     timeLimitUnits = 'seconds'
